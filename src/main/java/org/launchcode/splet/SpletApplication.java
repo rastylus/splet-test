92d952277b67b352b4235189b389a2ea0f22ff6f
rastylus <ryan.p.riggs@gmail.com>
2023-11-28 08:50:44 -0600

    initial spring boot

---
+++ src/main/java/org/launchcode/splet/SpletApplication.java
@@ -0,0 +1,13 @@
+package org.launchcode.splet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpletApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpletApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/launchcode/splet/SpletApplicationTests.java
@@ -0,0 +1,13 @@
+package org.launchcode.splet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpletApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
